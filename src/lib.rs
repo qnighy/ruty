07582fa6f0338a8e8afda8799a5a58bfb00b605b
Masaki Hara <ackie.h.gmai@gmail.com>
2024-12-24 22:50:39 +0900

    Add nodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, Default)]
+pub struct Ctx {
+    pub fresh_id: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Expr {
+    Integer(IntegerExpr),
+    Write(WriteExpr),
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct IntegerExpr {
+    pub value: i32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct WriteExpr {
+    pub lhs: Box<WriteTarget>,
+    pub rhs: Box<Expr>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum WriteTarget {
+    LocalVariable(LocalVariableWriteTarget),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct LocalVariableWriteTarget {
+    pub name: String,
+    pub type_annotation: Option<Type>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Type {
+    Integer,
 }
