224953825a01229dade93f19c9100a8a3f5ceecd
Masaki Hara <ackie.h.gmai@gmail.com>
2025-02-02 21:34:15 +0900

    Switch to the new type checker

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod ast;
-mod checker;
 mod checker2;
 pub mod encoding;
 mod eraser;
@@ -9,8 +8,7 @@ mod parser;
 mod util;
 
 use crate::ast::CodeRange;
-pub use checker::typecheck_program;
-pub use checker2::typecheck_program as typecheck_program2;
+pub use checker2::typecheck_program;
 pub use encoding::{CharPlus, EString, Encoding};
 pub use eraser::erase_type;
 use liveness::liveness_analysis;
